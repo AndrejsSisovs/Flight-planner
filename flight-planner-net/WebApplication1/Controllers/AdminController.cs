9f5e0025694a17074871be92da552787a4589c78
DESKTOP-22JD16V\Андрей <vankon2010@gmail.com>
2024-10-13 21:36:40 +0300

    first test done half

---
+++ flight-planner-net/WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("admin-api")]
+    [ApiController]
+    [Authorize]
+    public class AdminController : ControllerBase
+    {
+        
+        [Route("flights/{id}")]
+        [HttpGet]
+        public IActionResult GetFlight(int id)
+        {
+            return NotFound();
+        }
+    }
+}
